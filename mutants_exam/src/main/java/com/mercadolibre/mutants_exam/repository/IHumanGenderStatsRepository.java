c56a9bd9c925af2faa965e92ede0816145e128c9
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2020-06-02 20:26:16 -0300

    Adding entities and repositories for Human_Gender and Human_Gender_Stats

---
+++ mutants_exam/src/main/java/com/mercadolibre/mutants_exam/repository/IHumanGenderStatsRepository.java
@@ -0,0 +1,6 @@
+package com.mercadolibre.mutants_exam.repository;
+
+import com.mercadolibre.mutants_exam.entity.HumanGenderStat;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IHumanGenderStatsRepository extends CrudRepository<HumanGenderStat, Long> { }
