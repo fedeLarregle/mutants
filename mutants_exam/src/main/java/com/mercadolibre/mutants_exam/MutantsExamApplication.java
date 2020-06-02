d7fa1ca698c5261263ca26e8d66dc0c4ff898ef4
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2020-06-02 19:54:45 -0300

    Initializing project.

---
+++ mutants_exam/src/main/java/com/mercadolibre/mutants_exam/MutantsExamApplication.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.mutants_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantsExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantsExamApplication.class, args);
+	}
+
+}
