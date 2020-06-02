c74ab9dd0b5df23c58fb66c1a5652f28bde4adb5
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2020-06-02 20:45:36 -0300

    Adding HumanGenderStatsService

---
+++ mutants_exam/src/main/java/com/mercadolibre/mutants_exam/service/stats/IHumanGenderStatsService.java
@@ -0,0 +1,9 @@
+package com.mercadolibre.mutants_exam.service.stats;
+
+
+import com.mercadolibre.mutants_exam.dto.HumanGenderStats;
+
+public interface IHumanGenderStatsService {
+
+    HumanGenderStats getStats();
+}
